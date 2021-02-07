bb5d255faed61890e017e612f04389bd8521e1a4
nirajan <nira0101@stud.kea.dk>
2021-02-07 19:43:09 +0100

    same

--- basic.js
+++ basic.js
@@ -1,1 +1,42 @@
-console.log()
+"use strict";
+
+/* const bool = "true";
+const num = 41;
+const stir = "peter";
+const obj = {
+  cats: 2,
+  cars: 1,
+};
+const nothing = null;
+let undf;
+const symbol = Symbol("symbol");
+ */
+
+/* const n1 = 1;
+const n2 = 2;
+
+const s1 = "1";
+const s2 = "2";
+
+n1 + n2;
+s1 + s2;
+
+n1 + s2;
+s1 + n2; */
+
+const vlaue = "";
+
+if (vlaue) {
+  console.log("value is right");
+} else {
+  console.log("value is wrong");
+}
+
+/* "" + 1 + 0;
+"" - 1 + 0;
+true + false;
+6 / -3;
+"2" * "3";
+4 + 5 + "px";
+"$" + 4 + 5;
+"4" - 2; */
